bbd10290347bde175dc557d10380d514e995d867
Martin Krupa <martin.krupa@adwise.cz>
2023-11-13 16:29:15 +0100

    Added documentation to Operations API

--- octopus-yjs-libs/src/api/YJsScriptOperations.ts
+++ octopus-yjs-libs/src/api/YJsScriptOperations.ts
@@ -6,10 +6,12 @@ import { SlateParagraph, SlateScript, SlateScriptElement } from '../types/SlateS
 import { mapObjectToXMLText, mapSlateScriptToInsertDeltas } from '../mappers/YJSScriptMapper';
 
 /**
- * 
- * @param sharedType 
- * @param elementIndex 
- * @returns 
+ * According to provided elementIndex and eventually a paragraphIndex, returns a sharedType's Y.XMLText object of either SlateScriptElement or SlateParagraph. 
+ * It's a helper function used to execute further change operations on the Y.XMLText object
+ * @param sharedType - YJS Document carrying Slate's Document encoded as Y.XMLText  
+ * @param elementIndex - index of element to look for in the sharedType 
+ * @param paragraphIndex - index of paragraph within the element to look for in the sharedType
+ * @returns - Y.XMLText of a Paragraph if elementIndex and paragraphIndex were provided. If only elementIndex was provided, then Y.XMLText of an Element is returned 
  */
 function getElementByIndexNo(sharedType: Y.XmlText, elementIndex: number, paragraphIndex?: number): Y.Text {
     const yElement = sharedType.toDelta() && sharedType.toDelta().length > elementIndex && sharedType.toDelta()[elementIndex].insert ? sharedType.toDelta()[elementIndex].insert : null;
@@ -21,10 +23,13 @@ function getElementByIndexNo(sharedType: Y.XmlText, elementIndex: number, paragr
 }
 
 /**
- * Adds a character onto a given position
- * @param elementIndex 
- * @param position 
- * @param sharedType 
+ * Adds a character/string onto a given position
+ * @param sharedType - YJS Document carrying Slate's Document encoded as Y.XMLText  
+ * @param elementIndex - 0 based index of afffected script element
+ * @param paragraphIndex - 0 based index of afffected script paragraph
+ * @param position - 0 based index of position within the paragraph's text onto which character would be inserted
+ * @param character - character/string to insert
+ * @param format - Object of any attributes and their values to be applied to the text paragraph. e.g {bold:true}
  */
 export function addCharacter(sharedType: Y.XmlText, elementIndex: number, paragraphIndex: number, character: string, position: number, format?: Object) {
     //WHEN ADDING TEXTS, shared Type has to be transformed to deltas to pick correct Element for manipulation
@@ -37,12 +42,11 @@ export function addCharacter(sharedType: Y.XmlText, elementIndex: number, paragr
 };
 
 /**
- * 
- * @param sharedType 
- * @param elementIndex 
- * @param paragraphIndex 
- * @param attributeName 
- * @param value 
+ * Sets attributes of a script's paragraph. This is a Slate document, so it's not intended for the Text nodes if text paragragraphs. For formatting the text use formatRange function
+ * @param sharedType - YJS Document carrying Slate's Document encoded as Y.XMLText 
+ * @param elementIndex - 0 based index of afffected script element
+ * @param paragraphIndex - 0 based index of afffected script paragraph
+ * @param attributes - Map of <name=>value> name of the attribute to set / remove. If value is null, the attibute is removed.
  */
 export function setParagraphAttributes(sharedType: Y.XmlText, elementIndex: number, paragraphIndex: number, attributes: Map<string, string | number | boolean>) {
     //WHEN ADDING TEXTS, shared Type has to be transformed to deltas to pick correct Element for manipulation
@@ -62,13 +66,13 @@ export function setParagraphAttributes(sharedType: Y.XmlText, elementIndex: numb
 };
 
 /**
- * 
- * @param sharedType 
- * @param elementIndex 
- * @param paragraphIndex 
- * @param position 
- * @param length 
- * @param format 
+ * Formats a text node (or a part of it) in a text Paragraph
+ * @param sharedType - YJS Document carrying Slate's Document encoded as Y.XMLText 
+ * @param elementIndex - 0 based index of afffected script element
+ * @param paragraphIndex - 0 based index of afffected script paragraph
+ * @param position - 0 based index of position within the paragraph's text from which format would be applied 
+ * @param length - number of characters in the paragraph onto which format would be applied starting at the position
+ * @param format - Object of any attributes and their values to be applied to the text paragraph. e.g {bold:true}
  */
 export function formatRange(sharedType: Y.XmlText, elementIndex: number, paragraphIndex: number, position: number, length: number, format: Object) {
     //WHEN ADDING TEXTS, shared Type has to be transformed to deltas to pick correct Element for manipulation
@@ -81,9 +85,9 @@ export function formatRange(sharedType: Y.XmlText, elementIndex: number, paragra
 };
 
 /**
- * Deletes an element
- * @param elementIndex 
- * @param sharedType 
+ * Deletes an element from a shared type
+ * @param sharedType - YJS Document carrying Slate's Document encoded as Y.XMLText 
+ * @param elementIndex - 0 based index of afffected script element
  */
 export function deleteElement(sharedType: Y.XmlText, elementIndex: number) {
     //The element are then the top level deltas.
@@ -94,12 +98,12 @@ export function deleteElement(sharedType: Y.XmlText, elementIndex: number) {
 };
 
 /**
- * 
- * @param sharedType 
- * @param elementIndex 
- * @param paragraphIndex 
- * @param position 
- * @param length 
+ * Deletes character(s) from a text node of a text paragraph
+ * @param sharedType - YJS Document carrying Slate's Document encoded as Y.XMLText 
+ * @param elementIndex - 0 based index of afffected script element
+ * @param paragraphIndex - 0 based index of afffected script paragraph
+ * @param position -  0 based index of position within the paragraph's text from which the delete starts 
+ * @param length - number of characters in the paragraph to remove from the position
  */
 export function deleteCharacter(sharedType: Y.XmlText, elementIndex: number, paragraphIndex: number, position: number, length: number = 1) {
     //WHEN ADDING TEXTS, shared Type has to be transformed to deltas to pick correct Element for manipulation
@@ -111,6 +115,12 @@ export function deleteCharacter(sharedType: Y.XmlText, elementIndex: number, par
     }
 };
 
+/**
+ * Deletes a paragraph from an element of a sharedObject
+ * @param sharedType - YJS Document carrying Slate's Document encoded as Y.XMLText 
+ * @param elementIndex - 0 based index of afffected script element
+ * @param paragraphIndex - 0 based index of afffected script paragraph
+ */
 export function deleteParagraph(sharedType: Y.XmlText, elementIndex: number, paragraphIndex: number) {
     //WHEN ADDING TEXTS, shared Type has to be transformed to deltas to pick correct Element for manipulation
     //sharedType is the YXML content of YJS 
@@ -122,10 +132,10 @@ export function deleteParagraph(sharedType: Y.XmlText, elementIndex: number, par
 };
 
 /**
- * 
- * @param sharedType 
- * @param element 
- * @param index 
+ * Transforms Octopus Element into Slate's Element and inserts it into YJS Document as a change
+ * @param sharedType - YJS Document carrying Slate's Document encoded as Y.XMLText 
+ * @param element - Octopus Script Element Object
+ * @param index - position to insert an element onto - a 0 - based index
  */
 export function addElement(sharedType: Y.XmlText, element: OctopusScriptElement, index: number) {
     const yElement: Y.XmlText = new Y.XmlText();
@@ -137,6 +147,13 @@ export function addElement(sharedType: Y.XmlText, element: OctopusScriptElement,
     sharedType.applyDelta([{retain: index}, { insert: mapObjectToXMLText(slateElement) }]);
 };
 
+/**
+ * Transforms Octopus Paragraph into Slate's Paragraph and inserts it into YJS Document as a change
+ * @param sharedType - YJS Document carrying Slate's Document encoded as Y.XMLText 
+ * @param elementIndex - 0 based index of afffected script element
+ * @param paragraph -Object of  Octopus Script Paragraph to insert 
+ * @param index - position to insert an paragraph into within an element - a 0 - based index
+ */
 export function addParagraph(sharedType: Y.XmlText, elementIndex: number, paragraph: OctopusScriptParagraph, index: number) {
 
     const yElement: Y.Text = getElementByIndexNo(sharedType, elementIndex);
@@ -150,13 +167,11 @@ export function addParagraph(sharedType: Y.XmlText, elementIndex: number, paragr
 };
 
 /**
- * 
- * @param sharedType 
- * @param elementIndex 
- * @param attributeName 
- * @param value 
+ * Sets attributes of a script's element.
+ * @param sharedType - YJS Document carrying Slate's Document encoded as Y.XMLText 
+ * @param elementIndex - 0 based index of afffected script element
+ * @param attributes - Map of <string, string | number | boolean> for attributes and their values to be set on the element. If value is null, the attribute is removed.
  */
-
 export function setElementAttributes(sharedType: Y.XmlText, elementIndex: number, attributes: Map<string, string | number | boolean>) {
     const yElement: Y.Text = sharedType.toDelta() && sharedType.toDelta().length > elementIndex && sharedType.toDelta()[elementIndex].insert ? sharedType.toDelta()[elementIndex].insert : null;
     if (yElement) {
@@ -173,6 +188,11 @@ export function setElementAttributes(sharedType: Y.XmlText, elementIndex: number
     }
 };
 
+/**
+ * Transforms Octopus Script into Slate's Script and inserts it into YJS Document as a change
+ * @param sharedType - YJS Document carrying Slate's Document encoded as Y.XMLText 
+ * @param octopusScript - Object of  Octopus Script to insert 
+ */
 export function initializeSharedTypeFromOctopusScript(sharedType: Y.XmlText, octopusScript: OctopusScript) {
 
     let slateScript = mapOctopusScriptToSlateScript(octopusScript);
@@ -182,9 +202,9 @@ export function initializeSharedTypeFromOctopusScript(sharedType: Y.XmlText, oct
 }
 
 /**
- * 
- * @param sharedType 
- * @param changeArray 
+ * Applies updates retrieved from the outside world onto the shared YJS Document
+ * @param sharedType - YJS Document carrying Slate's Document encoded as Y.XMLText 
+ * @param changeArray - byte array carrying the changes. See https://docs.yjs.dev/api/document-updates 
  */
 export function updateSharedTypeFromAS(sharedType: Y.XmlText, changeArray: []) {
     Y.applyUpdate(sharedType.doc, arrayToUint8Array(changeArray));
--- yjs-webpack/src/yjs-test.ts
+++ yjs-webpack/src/yjs-test.ts
@@ -1,18 +1,16 @@
 import * as Y from 'yjs';
-import {XmlText} from 'yjs';
 import {getTestScript} from "octopus-yjs-libs/data/TestScript";
 import * as yOps from "octopus-yjs-libs/api/YJsScriptOperations"
 import { YJsScriptChangeListener } from 'octopus-yjs-libs/api/YJsScriptChangeListener';
 
 
 export function testYJS(){
-    // Yjs documents are collections of
-    // shared objects that sync automatically.
+    // Yjs documents are collections of shared objects that sync automatically.
     const ydoc = new Y.Doc()
     //Dummy data
     const mockOctopusScript = getTestScript();
     // Create a sharedType of Y.XmlText
-    const sharedOctopusScript: any = ydoc.get("content", XmlText);
+    const sharedOctopusScript = ydoc.get("content", Y.XmlText) as Y.XmlText;
     //Listen to changes
     const changeListener: YJsScriptChangeListener = new YJsScriptChangeListener(sharedOctopusScript, (changes)=>{
         console.log("Observed some changes on the shared document", changes);
